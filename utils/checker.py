960e2f3e658c2cf10399e94c9f58e8d3f75791dd
IgorEfimof <82845934+IgorEfimof@users.noreply.github.com>
2025-04-28 07:49:33 +0300

    Update checker.py

--- utils/checker.py
+++ utils/checker.py
@@ -1,3 +1,6 @@
 def is_arbitrage(odd1, odd2, odd3):
+    """
+    Проверка: сумма вероятностей меньше 1 -> арбитраж возможен
+    """
     probability_sum = (1 / odd1) + (1 / odd2) + (1 / odd3)
     return probability_sum < 1
